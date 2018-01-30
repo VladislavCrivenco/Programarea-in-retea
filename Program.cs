26d6e55686fc1f16b39fcdcc507aeb3550db72ab
Vladislav <vladislav1996-07@mail.ru>
2018-01-30 09:49:34 +0200

    Add Hello World Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programarea_in_retea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
